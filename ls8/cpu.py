11a951e3af2f38a80847155cfd25b1bdc7aac345
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-03-23 23:56:01 -0500

    mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,41 +2,35 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
 
-    def load(self):
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
+
+    def load(self, file):
         """Load a program into memory."""
-
-        address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        with open(file) as f:
+            for x in f:
+                self.ram[self.pc] = int(x[0:8], 2)
+                self.pc += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +42,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +56,24 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.pc = 0
+        IR = self.ram[self.pc]
+        operand_a = self.ram[self.pc + 1]
+        operand_b = self.ram[self.pc + 2]
+        HRT = 1
+        LDI = 82
+        PRN = 47
+
+        while True:
+            if IR == HRT:
+                break
+            elif IR == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+                IR = self.ram[self.pc]
+            elif IR == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+                IR = self.ram[self.pc]
+            self.pc += 1
+            IR = self.ram[self.pc]
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
+
 """Main."""
 
 import sys
 from cpu import *
 
+if len(sys.argv) < 2:
+    sys.exit("ERROR: No CL Input")
+
 cpu = CPU()
 
-cpu.load()
+cpu.load(sys.argv[1])
 cpu.run()
